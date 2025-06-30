0f9ba8d8ed02c80f0c8e712f04223b48385f40d7
amin <noramin@infopro.com.my>
2025-06-30 17:46:40 +0800

    update LLM function

--- src/msteamuat/llm.py
+++ src/msteamuat/llm.py
@@ -3,7 +3,8 @@ import os
 from langchain_ollama import OllamaLLM
 
 def get_llm():
-    return Ollama(
+    return OllamaLLM(
         model=os.getenv("MODEL", "mistral:7b"),
-        base_url=os.getenv("OLLAMA_API_BASE", "http://10.10.1.187:11434")
+        base_url=os.getenv("OLLAMA_API_BASE", "http://10.10.1.187:11434"),
+        temperature=0.7,
     )
